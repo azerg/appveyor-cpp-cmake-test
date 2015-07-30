ebc0af0c524593632ab768fdc40186bec5fbdc86
azerg <pub@azerg.com>
2015-07-30 14:17:28 +0300

    test

--- build_system/appveyor.helper.py
+++ build_system/appveyor.helper.py
@@ -4,7 +4,7 @@
 import os
 from subprocess import check_call
 
-message("hello from appveyor.helper.py")
+print "hello from appveyor.helper.py"
 
 build = os.environ['BUILD']
 config = os.environ['CONFIG']
